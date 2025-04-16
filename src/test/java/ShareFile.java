ef3b2645da47a40d1fd5b9a3462882d8bbbf6d0c
nhatthuy <nhatthuyld@gmail.com>
2025-04-16 16:28:10 +0700

    change xpath

--- src/test/java/ShareFile.java
+++ src/test/java/ShareFile.java
@@ -13,22 +13,21 @@ public class ShareFile {
 
     public ShareFile(WebDriver driver) {
         this.driver = driver;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
    public void open_Url(String url_string)
    {
        driver.get(url_string);
    }
-    public void findXpath(String xpath_string)
-    {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.findElement(By.xpath(xpath_string));
+    public WebElement findXpath(String xpath_string) {
+        return (driver.findElement(By.xpath(xpath_string)));
     }
 
-    public void clickButton(String xpath_string)
-    {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath_string)));
-        loginBtn.click();
+    public WebElement findByID(String id_string) {
+        return (driver.findElement(By.id(id_string)));
     }
+
+
 
 }
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class Test1{
 
@@ -21,10 +24,19 @@ public class Test1{
     public void testCase1() {
         ShareFile sf = new ShareFile(driver);
         sf.open_Url("https://www.utest.com/");
-        sf.clickButton("//a[@class='unauthenticated-nav-bar-new__sign-up']");
-        sf.clickButton("//button[@id='onetrust-accept-btn-handler']");
-        sf.clickButton("//form[@name='userForm']//button[@class='btn btn-blue']");
+        sf.findXpath("//button[@id='onetrust-accept-btn-handler']").click();
+        sf.findXpath("//a[@class='unauthenticated-nav-bar-new__sign-up']").click();
+        sf.findXpath("//form[@name='userForm']//button[@class='btn btn-blue']").click();
 
+        //Step 3: Verify warning message display correctly
+        String firstname_message = sf.findByID("firstNameError").getText();
+        assertTrue("firstname error not found!", firstname_message.contains("First name is required"));
+
+        String lastname_message = sf.findByID("lastNameError").getText();
+        assertTrue("lastnamerror not found!", lastname_message.contains("Last name is required"));
+
+        String Emailerror_message = sf.findByID("emailError").getText();
+        assertTrue("email error not found!", Emailerror_message.contains("Enter valid email"));
 
     }
 
